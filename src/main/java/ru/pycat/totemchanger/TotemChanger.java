ef94d95b4d6ee4cb873ddf48b274182635a94cf5
pycat <106306334+pycatmc@users.noreply.github.com>
2023-02-10 17:10:43 +0900

    cloth auto config

--- src/main/java/ru/pycat/totemchanger/TotemChanger.java
+++ src/main/java/ru/pycat/totemchanger/TotemChanger.java
@@ -1,30 +1,38 @@
 package ru.pycat.totemchanger;
 
-import com.google.gson.JsonObject;
 import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.loader.api.FabricLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import static com.terraformersmc.modmenu.ModMenu.GSON;
+import ru.pycat.totemchanger.config.TotemChangerConfig;
 
 public class TotemChanger implements ModInitializer {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("totemchanger");
-
     public static TotemChangerConfig config;
 
     @Override
     public void onInitialize() {
-        AutoConfig.register(TotemChangerConfig.class, GsonConfigSerializer::new);
+        AutoConfig.register(TotemChangerConfig.class, JanksonConfigSerializer::new);
         config = AutoConfig.getConfigHolder(TotemChangerConfig.class).getConfig();
         LOGGER.info("Loaded!");
+    }
+
+    public boolean isEnabled() {
+        return this.config.enabled;
+    }
+
+    public float getScale() {
+        return this.config.scale;
+    }
+
+    public float getPosX() {
+        return this.config.posX;
+    }
+
+    public float getPosY() {
+        return this.config.posY;
     }
 
 }
--- src/main/java/ru/pycat/totemchanger/config/TotemChangerModMenu.java
+++ src/main/java/ru/pycat/totemchanger/config/TotemChangerModMenu.java
@@ -1,12 +1,8 @@
-package ru.pycat.totemchanger;
+package ru.pycat.totemchanger.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import com.terraformersmc.modmenu.gui.ModsScreen;
 import me.shedaniel.autoconfig.AutoConfig;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.text.Text;
 
 public class TotemChangerModMenu implements ModMenuApi {
 
